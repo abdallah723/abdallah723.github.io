684e8a17d43db4c20e4e65348907f41260cbbe7b
abdallah723 <57788908+abdallah723@users.noreply.github.com>
2020-01-25 23:38:51 -0500

    Add files via upload
    Quick Quiz

--- script.js
+++ script.js
@@ -1,105 +1,75 @@
-// function generate (){
-  
+$(document).ready(function (){
+    var questions = [
+        {
+            title: "What does HTML stand for?",
+            choices: ["Hyper Text Markup Language", "High Text Markup Language", "Hyper Tabular Markup Lanuage", "High Tabular Markup Language"],
+            answer: "Hyper Text Markup Language"
+        },
 
-//   var characters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "@", "%", "+", "\\", "/", "'", "!","#", "$","^","?",":", ",", ")", "(", "}", "{","]","[","~","-","_",".", "a","b","c","d" , "e","f","g","h","i","j","k","l","m",
-//   "n",
-//   "o",
-//   "p",
-//   "q",
-//   "r",
-//   "s",
-//   "t",
-//   "u",
-//   "v",
-//   "w",
-//   "x",
-//   "y",
-//   "z", "A",
-//   "B",
-//   "C",
-//   "D",
-//   "E",
-//   "F",
-//   "G",
-//   "H",
-//   "I",
-//   "J",
-//   "K",
-//   "L",
-//   "M",
-//   "N",
-//   "O",
-//   "P",
-//   "Q",
-//   "R",
-//   "S",
-//   "T",
-//   "U",
-//   "V",
-//   "W",
-//   "X",
-//   "Y",
-//   "Z" ]
+        {
+            title: "Arrays can contain which of the following?",
+            choices: ["Strings", "Numbers", "Booleans", "All Of the Above"],
+            answer: "All Of the Above"
+        },
 
-//   var password = "";
-//     var array = ""
-//   for (var i = 0; i < 25; i++){
-//       password = password + characters(Math.floor(Math.random() * Math.floor(values.length -1)));
+        {
+            title: "Which of the following is the correct marking for begining a paragraph?",
+            choices: ["<p>", "<par>", "br", "txt"],
+            answer: "<p>"
+        },
 
-//       document.getElementById("textarea").value = password;
-//   }
-//   function copyPassword(){
-//       document.getElementById("password").Selecto();
-//       document.execCommand("copy");
-//       alert("Password Copied");
-      
-//   }
-//   document.getElementById("generateButton").addEventListener("click", function (copy){
-//       copy.preventDefault();
-//       generate();
-//   })
+        {
+            title: "What is the correct format to link your styles sheet to your index file?",
+            choices: ["src='style' href='filename.css'", "stylesheet href='filename.css'", "link='stylesheet' href='filename.css", "src='stylesheet' href='filename.css'"],
+            answer: "src='stylesheet' href='filename.css'"
 
-// var upper = arrayLowtoHigh(65,85);
-// var LowerCase = arrayLowtoHigh(97,122)
-// var numbers = arrayLowtoHigh(48,57)
-// var symbols = arrayLowtoHigh (33,47).concat(arrayLowtoHigh(58,64).concat(91,96).concat(arrayLowtoHigh(123,126)))
+        },
 
-// function generatePassword (){
-//     String.fromCharCode(65)
-//     var array = []
+        {
+            title: "What is the correct mark for executing JavaScript in your main index file?",
+            choices: ["<scrlink>", "<jscript>", "<scripting>", "<script>"],
+            answer: "<script>"
+        },
+    ];
+    $(".questions").text($(this).text());
+    
 
-//     function arrayLowtoHigh (low, high) {
-//         for(let i = low; i<= high; i++); {
-//         array.push = (i)
-//     }
-//     }
-//     return array
-// }
+    for (var i=0; i < questions.length; i++) {
+    console.log("title: " + questions[i].title, "choices: " + questions[i].choices);    
+    }
 
-var generatedPassword = document.getElementById("password");
+    timeEl = document.querySelector(".time");
+    var secondsLeft = 75;
+    function setTime() {
+        var timeInterval = setInterval(function (){
+            secondsLeft--;
+            timeEl.textContent = secondsLeft;
+
+            if(secondsLeft === 0) {
+                clearInterval(timeInterval);
+            }
+        }, 1000);
+    }
+
+    var mainEl = document.getElementById("main");
+    var timerEl = document.getElementById("countdown");
+    var bodyEl = document.getElementById("div");
+
+    function quiz() {
+        var timeInterval = setInterval(function () {
+            timerEl.textContent = timeLeft;
+            timeleft--;
+
+            if (timeLeft === 0) {
+                timerEl.textContent = "";
+                clearInterval(timeInterval);
+            }
+        }, 1000)
+    }
 
 
-var randomPass = {
-    lower: generateLower,
-    upper: generateUpper,
-    number: generateNumber,
-    symbols: generateSymbols
-};
 
 
-function generateLower(){
-    return String.fromCharCode(Math.floor(Math.random()*26)+97);
-}
 
-function generateUpper (){
-    return String.fromCharCode(Math.floor(Math.random()*26) + 65);
-}
 
-function generateNumber (){
-    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-}
-
-function generateSymbols (){
-    var symbols =  "@,%,+,\\,/,',!,#,$,^,?,:,,,),(,},{,],[,~,-,_,."
-    return symbols[Math.floor(Math.random()* symbols.length)]
-}
+});
