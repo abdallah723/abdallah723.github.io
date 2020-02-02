e43b39e9ac0dae7ce4efcdb55ac3e2cd2254634b
abdallah723 <57788908+abdallah723@users.noreply.github.com>
2020-02-01 23:45:59 -0500

    Add files via upload
    daily planner

---
+++ planner_script.js
@@ -0,0 +1,6 @@
+let time = moment();
+console.log(time);
+// let m = moment().format('MMMM Do YYYY, h:mm:ss a');
+// console.log(m);
+// time.text("class", "time"); --> get the time and date to show up (something is not working right)
+
